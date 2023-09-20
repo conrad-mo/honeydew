b88bdc431a134418e48e81ae4e99bd7efeb26925
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-09-20 12:06:01 -0400

    Separate different pages as components because I want it under 1 url

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import Image from 'next/image'
-
+import './page.css'
+import HomeScreen from "../components/homescreen";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-center items-center gap-16">
-      <div className='text-8xl'>Honeydew</div>
-      <div>Generate the perfect cover letters</div>
-      <button className='hover:bg-green-600 p-1'>Start</button>
+    <main className="flex justify-center min-h-screen">
+      <HomeScreen></HomeScreen>
     </main>
   )
 }
---
+++ components/homescreen.tsx
@@ -0,0 +1,9 @@
+export default function HomeScreen() {
+  return (
+    <main className="flex  flex-col justify-center items-center gap-14">
+      <div className='text-8xl'>Honeydew</div>
+      <div>Generate the perfect cover letters</div>
+      <button className='hover:bg-honeydew-dark  p-1'>Start</button>
+    </main>
+  )
+}
