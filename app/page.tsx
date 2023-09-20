22050d0aa6b1bea8828789350b9f5a790cc86539
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-09-20 01:39:16 -0400

    I have no UI in mind

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-center items-center gap-24">
-      <div className=''>Honeydew</div>
-      <button>Start</button>
+    <main className="flex min-h-screen flex-col justify-center items-center gap-16">
+      <div className='text-8xl'>Honeydew</div>
+      <div>Generate the perfect cover letters</div>
+      <button className='hover:bg-green-600 p-1'>Start</button>
     </main>
   )
 }
