508cd8a42bbb3477988cc83223d0f132ba79e766
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-09-20 01:08:17 -0400

    Sample stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Image from 'next/image'
+import Image from "next/image";
+import * as React from "react"
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <main className="">
-      
+      <div>
+        <Button>Start</Button>
+      </div>
     </main>
-  )
+  );
 }
